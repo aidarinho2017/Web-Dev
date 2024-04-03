84358610d817fb2d19b14d20a34515e80a51779b
aidarinho2017 <85069285+aidarinho2017@users.noreply.github.com>
2024-04-03 18:00:15 +0500

    Add files via upload

---
+++ kazakTeam-frontend-main/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AccountComponent} from "./account/account.component";
+import {FavoritesComponent} from "./favorites/favorites.component";
+import {PurchasesComponent} from "./purchases/purchases.component";
+import {CategoriesComponent} from "./categories/categories.component";
+import {ProductDetailComponent} from "./product-detail/product-detail.component";
+import { SearchComponent } from './search/search.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { RegistrationComponent } from './registration/registration.component';
+import {AuthoGuard} from "./autho.guard";
+import {BotComponent} from "./bot/bot.component";
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'account', component: AccountComponent, canActivate: [AuthoGuard]},
+  {path: 'favorites', component: FavoritesComponent},
+  {path: 'purchases', component: PurchasesComponent},
+  {path: 'categories/:id/products', component: CategoriesComponent},
+  {path: 'categories/:category_id/products/:product_id', component: ProductDetailComponent},
+  {path: 'search/:title', component: SearchComponent},
+  {path: 'login', component: AuthorizationComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: "bot", component: BotComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation:'reload'})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ kazakTeam-frontend-main/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AccountComponent } from './account/account.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { PurchasesComponent } from './purchases/purchases.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { FormsModule } from '@angular/forms';
+import { SearchComponent } from './search/search.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { RegistrationComponent } from './registration/registration.component';
+import {AuthoGuard} from "./autho.guard";
+import { HttpClientModule} from "@angular/common/http";
+import { BotComponent } from './bot/bot.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AccountComponent,
+    FavoritesComponent,
+    PurchasesComponent,
+    CategoriesComponent,
+    ProductDetailComponent,
+    SearchComponent,
+    AuthorizationComponent,
+    RegistrationComponent,
+    BotComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+
+  ],
+  providers: [
+    AuthoGuard,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
